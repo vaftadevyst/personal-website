280f09436402d60d311678079b332101cbba39f8
vaftadevyst <vaftadevyst@gmail.com>
2022-07-27 00:27:49 +0700

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,151 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project1.png";
+import projImg11 from "../assets/img/project1.1.png";
+import projImg12 from "../assets/img/project1.2.png";
+import projImg2 from "../assets/img/project2.png";
+import projImg21 from "../assets/img/project2.1.png";
+import projImg3 from "../assets/img/project3.png";
+import projImg31 from "../assets/img/project3.1.png";
+import projImg4 from "../assets/img/project4.png";
+import projImg41 from "../assets/img/project4.1.png";
+import projImg42 from "../assets/img/project4.2.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects1 = [
+    {
+      title: "Mini Portfolio Website",
+      description: "Web Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Mini Portfolio Website",
+      description: "Web Development",
+      imgUrl: projImg11,
+    },
+    {
+      title: "Mini Portfolio Website",
+      description: "Web Development",
+      imgUrl: projImg12,
+    },
+    {
+      title: "Resume Website",
+      description: "Web Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Resume Website",
+      description: "Web Development",
+      imgUrl: projImg31,
+    }
+  ];
+  const projects2 = [
+    {
+      title: "Profile Company Website",
+      description: "Web Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Profile Company Website",
+      description: "Web Development",
+      imgUrl: projImg21,
+    }
+  ];
+  const projects3 = [
+    {
+      title: "Dashboard Modern Design",
+      description: "Dashboard Creation",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Dashboard Modern Design",
+      description: "Dashboard Creation",
+      imgUrl: projImg41,
+    },
+    {
+      title: "Dashboard Modern Design",
+      description: "Dashboard Creation",
+      imgUrl: projImg42,
+    }
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Recent Our Project</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Personal Website</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Profile Website</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Dashboard Modern</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects1.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,80 @@
+import tableau from "../assets/img/3.png";
+import ads from "../assets/img/0.png";
+import excel from "../assets/img/1.png";
+import python from "../assets/img/2.png";
+import html from "../assets/img/html.png";
+import css from "../assets/img/css.png";
+import js from "../assets/img/js.png";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Service</h2>
+                        <p>our services include web development using HTML, CSS & Javascript, machine learning using python, data analysis using excel, business intelligence using tableau and facebook ads</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={python} alt="Image" />
+                                <h5>Python</h5>
+                            </div>
+                            <div className="item">
+                                <img src={tableau} alt="Image" />
+                                <h5>Tableau</h5>
+                            </div>
+                            <div className="item">
+                                <img src={excel} alt="Image" />
+                                <h5>Excel</h5>
+                            </div>
+                            <div className="item">
+                                <img src={html} alt="Image" />
+                                <h5>HTML</h5>
+                            </div>
+                            <div className="item">
+                                <img src={css} alt="Image" />
+                                <h5>CSS</h5>
+                            </div>
+                            <div className="item">
+                                <img src={js} alt="Image" />
+                                <h5>Javascript</h5>
+                            </div>
+                            <div className="item">
+                                <img src={ads} alt="Image" />
+                                <h5>FB Ads</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  )
+}
